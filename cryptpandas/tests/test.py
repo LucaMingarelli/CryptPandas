ba276995817afcf7cc42f4dc764a7d1ba13f4b92
Luca Mingarelli <lucamingarelli@me.com>
2021-12-12 00:43:44 +0100

    Create test.py

---
+++ cryptpandas/tests/test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import cryptpandas as crp
+
+df = pd.DataFrame({'A': [1, 2, 3],
+                   'B': ['one', 'one', 'four']})
+
+def test_read_write():
+  crp.to_encrypted(df, password='mypassowrd123', path='file.crypt')
+  decrypted_df = crp.read_encrypted(path='file.crypt', password='mypassowrd123')
+  assert (df == decrypted_df).all().all()
