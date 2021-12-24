77fdc276d74849b93516b5acec668d334ff3107c
Luca Mingarelli <lucamingarelli@me.com>
2021-12-24 04:53:46 +0100

    Update test_IO.py

--- cryptpandas/tests/test_IO.py
+++ cryptpandas/tests/test_IO.py
@@ -6,8 +6,19 @@ df = pd.DataFrame({'A': [1, 2, 3],
 
 
 class TestReadWrite:
+  def test_import(self):
+    import cryptpandas as crp
+    assert crp
+  
   def test_read_write(self):
     crp.to_encrypted(df, password='mypassowrd123', path='file.crypt')
     decrypted_df = crp.read_encrypted(path='file.crypt', password='mypassowrd123')
     assert (df == decrypted_df).all().all()
+  
+  def test_with_salt(self):
+    my_salt = crp.make_salt(32)
+    crp.to_encrypted(df, password='mypassword123', path='file.crypt', salt=my_salt)
+    decrypted_df = crp.read_encrypted(path='file.crypt', password='mypassword123', salt=my_salt)
+    assert (df == decrypted_df).all().all()
+  
 
