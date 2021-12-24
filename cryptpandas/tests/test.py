2dc74a71a34b9a4c73da9f32fadcbb18ea593443
Luca Mingarelli <lucamingarelli@me.com>
2021-12-24 03:26:48 +0100

    Update test.py

--- cryptpandas/tests/test.py
+++ cryptpandas/tests/test.py
@@ -8,8 +8,8 @@ def test_read_write():
   
   
 class TestReadWrite:
-    def test_read_write(self):
-        crp.to_encrypted(df, password='mypassowrd123', path='file.crypt')
-        decrypted_df = crp.read_encrypted(path='file.crypt', password='mypassowrd123')
-        assert (df == decrypted_df).all().all()
+  def test_read_write(self):
+    crp.to_encrypted(df, password='mypassowrd123', path='file.crypt')
+    decrypted_df = crp.read_encrypted(path='file.crypt', password='mypassowrd123')
+    assert (df == decrypted_df).all().all()
 
