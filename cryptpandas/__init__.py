5b093a8e6d8fb285175941a97922513cfa8a0586
Luca Mingarelli <lucamingarelli@me.com>
2024-03-04 21:05:46 +0000

    :rocket: Version 1.0.1 is out!

--- cryptpandas/__init__.py
+++ cryptpandas/__init__.py
@@ -4,7 +4,7 @@ Created on Tue Dec 22 00:11:40 2020
 @author: Luca Mingarelli
 """
 __about__ = "A tool for encryption and decryption of pandas dataframes."
-__version__= '1.0.0'
+__version__= '1.0.1'
 __author__ = 'Luca Mingarelli'
 __email__ = "lucamingarelli@me.com"
 __url__ = "https://github.com/LucaMingarelli/cryptpandas"
